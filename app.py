1419acb55aebaad840bf424e6e6b64c2dd86fae7
dbricare <dabricarello@ucdavis.edu>
2016-01-18 16:31:54 -0800

    Added gda graphic and reformat layout

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from bokeh.plotting import figure, output_file, save, show, ColumnDataSource
 from bokeh.models import HoverTool
 from bokeh.resources import CDN
-from bokeh.embed import components
+from bokeh.embed import components, autoload_static
 from datetime import date
 
 
@@ -13,71 +13,72 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return redirect('/index')
+    return redirect('/index')
 
 
 @app.route('/index', methods=['GET'])
 def index():  #remember the function name does not need to match the URL
 
-	# Load plot data
-	dfall = pd.read_csv('GeneDiseaseMoreCats.csv')
-	dfall.fillna(value='Not Available', inplace=True)
-	yy = dfall['Number of genes']
-	xx = dfall['score']
+    # Load plot data
+    dfall = pd.read_csv('GeneDiseaseMoreCats.csv')
+    dfall.fillna(value='Not Available', inplace=True)
+    yy = dfall['Number of genes']
+    xx = dfall['score']
 
-	# Generate colors for graphs
-	colorseq = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
-				'#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
-				'#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
-				'#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
-	colordark = colorseq[0::2]
-	colorlight = colorseq[1::2]
-	clrdrklt = colordark.copy()
-	clrdrklt.extend(colorlight)
-	uniqcat = dfall['category'].unique()
-	catclr = dict(zip(uniqcat.tolist(),clrdrklt[:len(uniqcat)]))
-	catclr['Not Available'] = '#eeeeee'
+    # Generate colors for graphs
+    colorseq = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
+                '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
+                '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
+                '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+    colordark = colorseq[0::2]
+    colorlight = colorseq[1::2]
+    clrdrklt = colordark.copy()
+    clrdrklt.extend(colorlight)
+    uniqcat = dfall['category'].unique()
+    catclr = dict(zip(uniqcat.tolist(),clrdrklt[:len(uniqcat)]))
+    catclr['Not Available'] = '#eeeeee'
 
-	# Scatter points for better readability
-	np.random.seed(1)
-	rndm = (np.random.random(yy.shape) - 0.5) * 0.8
-
-
-	# Generate plot
-	output_file("templates/index.html")
-
-	dfall['color'] = dfall['category'].map(lambda x: catclr[x])
-
-	source = ColumnDataSource(
-			data=dict(
-				x=xx,
-				y=yy+rndm,
-				genes=yy,
-				desc=dfall['diseaseName'],
-				cat=dfall['category']
-			)
-		)
-
-	hover = HoverTool(tooltips=[("Disease", "@desc"), ("Score", "$x"), ("Genes", "@genes"),("Category", "@cat")])
+    # Scatter points for better readability
+    np.random.seed(1)
+    rndm = (np.random.random(yy.shape) - 0.5) * 0.8
 
 
-	p = figure(plot_width=900, plot_height=600, tools=['box_zoom','pan','reset', 'save',hover], title="")
-	p.title_text_font = 'helvetica neue'
-	p.xaxis.axis_label = 'Strength of Association  ( Higher is Better )'
-	p.yaxis.axis_label = 'Gene Associations Per Disease  ( Lower is Better )'
-	p.xaxis.axis_label_text_font = 'helvetica neue'
-	p.yaxis.axis_label_text_font = 'helvetica neue'
+    # Generate plot
+    output_file("templates/index.html")
+
+    dfall['color'] = dfall['category'].map(lambda x: catclr[x])
+
+    source = ColumnDataSource(
+    data=dict(
+    x=xx,
+    y=yy+rndm,
+    genes=yy,
+    desc=dfall['diseaseName'],
+    cat=dfall['category']
+    )
+    )
+
+    hover = HoverTool(tooltips=[("Disease", "@desc"), ("Score", "$x"), ("Genes", "@genes"),("Category", "@cat")])
 
 
-	p.circle('x', 'y', size=20, fill_alpha=0.8, color=dfall['color'], source=source, line_width=1, line_color='#000000')
-	p.responsive = True
+    p = figure(plot_width=900, plot_height=600, tools=['box_zoom','pan','reset', 'save',hover], title="")
+    p.title_text_font = 'helvetica neue'
+    p.xaxis.axis_label = 'Strength of Association  ( Higher is Better )'
+    p.yaxis.axis_label = 'Gene Associations Per Disease  ( Lower is Better )'
+    p.xaxis.axis_label_text_font = 'helvetica neue'
+    p.yaxis.axis_label_text_font = 'helvetica neue'
 
-	script, div = components(p)
-	updated=date.today().strftime('%Y-%b-%d')
 
-	return render_template('index.html', script=script, div=div, updated=updated)
+    p.circle('x', 'y', size=20, fill_alpha=0.8, color=dfall['color'], source=source, line_width=1, line_color='#000000')
+    p.responsive = True
+
+#     script, div = autoload_static(p, CDN, "")
+    script, div = components(p)
+    updated=date.today().strftime('%Y-%b-%d')
+
+    return render_template('index.html', script=script, div=div, updated=updated)
 
 
 if __name__ == "__main__":
-	app.run(port=33507)
+    app.run(port=33507)
 
