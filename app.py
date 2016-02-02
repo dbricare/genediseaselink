1e191ec359c03aea56672c220f54e9ef26e47b00
dbricare <dabricarello@ucdavis.edu>
2016-02-02 13:59:38 -0800

    category selection

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect
 import numpy as np
 import pandas as pd
+import operator, os, datetime
 from bokeh.plotting import figure, output_file, save, show, ColumnDataSource
 from bokeh.models import HoverTool
 from bokeh.resources import CDN
 from bokeh.embed import components, autoload_static
-from datetime import date
 
 
 app = Flask(__name__)
@@ -13,19 +13,31 @@ app = Flask(__name__)
 
 catclr = {'Ear, nose, and throat': '#aec7e8', 'Endocrine system (hormones)': '#e377c2', 'Lungs and breathing': '#c49c94', 'Heart and circulation': '#17becf', 'Reproductive system': '#f7b6d2', 'Eyes and vision': '#9467bd', 'Digestive system': '#ff9896', 'Bones, muscles, and connective tissues': '#ff7f0e', 'Skin, hair, and nails': '#d62728', 'Immune system': '#ffbb78', 'Food, nutrition, and metabolism': '#2ca02c', 'Cancers': '#8c564b', 'Mouth and teeth': '#98df8a', 'Kidneys and urinary system': '#c5b0d5', 'Not Available': '#eeeeee', 'Blood/lymphatic system': '#bcbd22', 'Brain and nervous system': '#1f77b4'}
 
+disease_dict = {'all': 'All', 'digest': 'Digestive system', 'cancer': 'Cancers', 'skin': 'Skin, hair, and nails', 'heart': 'Heart and circulation', 'bone': 'Bones, muscles, and connective tissues', 'lung': 'Lungs and breathing', 'endocrine': 'Endocrine system (hormones)', 'brain': 'Brain and nervous system', 'reproductive': 'Reproductive system', 'kidney': 'Kidneys and urinary system', 'immune': 'Immune system', 'mouth': 'Mouth and teeth', 'metabolism': 'Food, nutrition, and metabolism', 'ent': 'Ear, nose, and throat', 'blood': 'Blood/lymphatic system', 'mental': 'Mental health and behavior', 'eye': 'Eyes and vision'}
+dislist = sorted(disease_dict.items(), key=operator.itemgetter(1))
 
 @app.route('/')
 def main():
     return redirect('/index')
 
 
-@app.route('/index', methods=['GET'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():  #remember the function name does not need to match the URL
 
     # Load plot data
 #     dfall = pd.read_csv('GeneDiseaseMoreCats.csv')
     dfall = pd.read_csv('ThreeGDA.tsv',sep='\t')
     dfall.fillna(value='Not Available', inplace=True)
+    
+    # return user-selected data
+    jump = ''
+    sel = ''
+    if request.method=='POST':
+        jump = '<script> window.location.hash="box"; </script>'
+        sel = request.form['selection']
+        if sel != 'all':
+            dfall = dfall[dfall['category']==disease_dict[sel]]
+    
     yy = dfall['Number of genes']
     xx = dfall['score_total']
 
@@ -79,10 +91,22 @@ def index():  #remember the function name does not need to match the URL
 
 #     script, div = autoload_static(p, CDN, "")
     script, div = components(p)
-#     updated=date.today().strftime('%Y-%b-%d')
-    updated = 'January 27, 2016'
 
-    return render_template('index.html', script=script, div=div, updated=updated)
+    t = os.path.getmtime('app.py')
+    t = os.path.getmtime('../genediseaselink-web/app.py')
+    updated = '{modt:%B} {modt.day}, {modt:%Y}'.format( 
+    modt=datetime.date.fromtimestamp(t) )
+
+#     updated = 'February 2, 2016'
+
+#     if request.method=='POST':
+#         return render_template(url_for('index',_anchor='box'), script=script, div=div, 
+#         dislist = dislist, updated=updated)
+#     else:
+
+
+    return render_template('index.html', script=script, div=div, 
+    dislist=dislist, updated=updated, jumpscript=jump, sel=sel)        
 
 
 if __name__ == "__main__":
