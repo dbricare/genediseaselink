c2a179cdf34a6f69dd29cb728d377ec8cddd22ef
dbricare <dabricarello@ucdavis.edu>
2016-02-01 18:10:14 -0800

    enhanced data analysis

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from datetime import date
 app = Flask(__name__)
 
 
+catclr = {'Ear, nose, and throat': '#aec7e8', 'Endocrine system (hormones)': '#e377c2', 'Lungs and breathing': '#c49c94', 'Heart and circulation': '#17becf', 'Reproductive system': '#f7b6d2', 'Eyes and vision': '#9467bd', 'Digestive system': '#ff9896', 'Bones, muscles, and connective tissues': '#ff7f0e', 'Skin, hair, and nails': '#d62728', 'Immune system': '#ffbb78', 'Food, nutrition, and metabolism': '#2ca02c', 'Cancers': '#8c564b', 'Mouth and teeth': '#98df8a', 'Kidneys and urinary system': '#c5b0d5', 'Not Available': '#eeeeee', 'Blood/lymphatic system': '#bcbd22', 'Brain and nervous system': '#1f77b4'}
+
+
 @app.route('/')
 def main():
     return redirect('/index')
@@ -20,23 +23,24 @@ def main():
 def index():  #remember the function name does not need to match the URL
 
     # Load plot data
-    dfall = pd.read_csv('GeneDiseaseMoreCats.csv')
+#     dfall = pd.read_csv('GeneDiseaseMoreCats.csv')
+    dfall = pd.read_csv('ThreeGDA.tsv',sep='\t')
     dfall.fillna(value='Not Available', inplace=True)
     yy = dfall['Number of genes']
-    xx = dfall['score']
+    xx = dfall['score_total']
 
     # Generate colors for graphs
-    colorseq = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
-                '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
-                '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
-                '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
-    colordark = colorseq[0::2]
-    colorlight = colorseq[1::2]
-    clrdrklt = colordark.copy()
-    clrdrklt.extend(colorlight)
-    uniqcat = dfall['category'].unique()
-    catclr = dict(zip(uniqcat.tolist(),clrdrklt[:len(uniqcat)]))
-    catclr['Not Available'] = '#eeeeee'
+#     colorseq = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
+#                 '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
+#                 '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
+#                 '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+#     colordark = colorseq[0::2]
+#     colorlight = colorseq[1::2]
+#     clrdrklt = colordark.copy()
+#     clrdrklt.extend(colorlight)
+#     uniqcat = dfall['category'].unique()
+#     catclr = dict(zip(uniqcat.tolist(),clrdrklt[:len(uniqcat)]))
+#     catclr['Not Available'] = '#eeeeee'
 
     # Scatter points for better readability
     np.random.seed(1)
